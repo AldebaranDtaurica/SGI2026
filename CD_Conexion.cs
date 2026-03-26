4f5225070ce08eb7b94210d1c7b8337e1114750c
AldebaranDtaurica <godoyismaelagustin@gmail.com>
2026-03-26 15:19:00 -0300

    Add files via upload

---
+++ CD_Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public abstract class CD_Conexion
+    {
+        private readonly string connectionstring;
+        private readonly string connectionStringMas;
+
+        public CD_Conexion()
+        {
+            
+		// Usamos el nombre del servidor GANDALF\SQLEXPRESS
+                connectionstring = "Server=GANDALF\\SQLEXPRESS;Database=SGInst_172;Integrated Security=True;TrustServerCertificate=True";
+            
+		// conexión a master con el servidor correcto
+          	 connectionStringMas = "Server=GANDALF\\SQLEXPRESS;Database=master;Integrated Security=True";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionstring);
+        }
+
+        protected SqlConnection GetConnectionMas()
+        {
+            return new SqlConnection(connectionStringMas);
+        }
+    }
+}
